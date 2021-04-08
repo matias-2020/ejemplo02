358318b9e2dcc9def0e48349126f15aeca15a315
matias-2020 <63679116+matias-2020@users.noreply.github.com>
2021-04-07 23:35:55 -0300

    primer commit

---
+++ dist/index.js
@@ -0,0 +1,51 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+//import express from 'express';
+//console.log("Hello World WORLD SURFERS"); al principio estaba esto que era test 
+const express_1 = __importDefault(require("express"));
+const morgan_1 = __importDefault(require("morgan"));
+const cors_1 = __importDefault(require("cors"));
+const indexRoutes_1 = __importDefault(require("./routes/indexRoutes"));
+const express_handlebars_1 = __importDefault(require("express-handlebars"));
+const path_1 = __importDefault(require("path"));
+class Server {
+    constructor() {
+        this.app = express_1.default();
+        this.config();
+        this.routes();
+    }
+    config() {
+        //Configuraciones
+        this.app.set('port', process.env.PORT || 3000);
+        this.app.set('views', path_1.default.join(__dirname, 'views')); //indicamos que views esta en dist y no en el modulo principal
+        this.app.engine('.hbs', express_handlebars_1.default({
+            defaultLayout: 'main',
+            layoutsDir: path_1.default.join(this.app.get('views'), 'layouts'),
+            partialsDir: path_1.default.join(this.app.get('views'), 'partials'),
+            extname: 'hbs',
+            helpers: require('./lib/handlebars') //definimos donde estan los helpers
+        }));
+        this.app.set('view engine', '.hbs'); //ejecutamos el modulo definido
+        //Middlewares
+        this.app.use(morgan_1.default('dev'));
+        this.app.use(cors_1.default()); //iniciamos cors
+        this.app.use(express_1.default.json()); //habilitamos el intercambio de objetos json entre aplicaciones
+        this.app.use(express_1.default.urlencoded({ extended: false })); //habilitamos para recibir datos a traves de formularios html.
+        //Variables globales
+    }
+    //ACA VAN TODAS LAS RUTAS DE LA APP
+    routes() {
+        this.app.use(indexRoutes_1.default);
+    }
+    start() {
+        this.app.listen(this.app.get('port'), () => {
+            console.log("Sever escuchando" + this.app.get('port'));
+        });
+    }
+}
+const server = new Server();
+server.start(); //Ejecutamos el metodo start en inica el server
+//# sourceMappingURL=index.js.map
